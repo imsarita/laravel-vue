ce1c82745a529c07f6fa558b77bd6ad22fa1ae5a
sarita68470 <sarita68470@gmail.com>
2023-03-13 18:14:02 +0530

    seeder

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,8 @@ use App\Http\Controllers\BookController;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
 Route::resource('/book',BookController::class)->only(['index','store','show','update','destroy']);
